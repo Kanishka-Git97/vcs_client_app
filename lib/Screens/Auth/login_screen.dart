f37dfb59eb3a085df021949fa547090c055d4357
Purna Kanishka <purnakanishka97@icloud.com>
2022-06-24 22:20:45 +0530

    Login Page UI Bug Fixed

--- lib/Screens/Auth/login_screen.dart
+++ lib/Screens/Auth/login_screen.dart
@@ -14,102 +14,105 @@ class _loginScreenState extends State<loginScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
         elevation: 0,
+        toolbarHeight: 5,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logoicon.png'),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 40,
-                    ),
-                  ),
-                  Text(
-                    'Veterinary Consultation Solution',
-                    style: TextStyle(fontSize: 15),
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logoicon.png'),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            const CustomTextField(
-                hintTxt: 'example@email.com', lableTxt: 'Email', mode: false),
-            const SizedBox(
-              height: 10,
-            ),
-            const CustomTextField(
-              hintTxt: '',
-              lableTxt: 'Password',
-              mode: true,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 60,
-              width: double.infinity,
-              child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      shadowColor: Colors.white,
-                      backgroundColor: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
                     ),
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                  height: 70,
-                  width: double.infinity,
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: 'Dont have an account?',
-                      style: TextStyle(color: Colors.grey[600]),
-                      children: [
-                        TextSpan(
-                          text: ' Register Here',
-                          style: TextStyle(color: Colors.blueAccent),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => OnBoard()));
-                            },
-                        ),
-                      ],
+                    Text(
+                      'Veterinary Consultation Solution',
+                      style: TextStyle(fontSize: 15),
                     ),
-                  )),
-            ),
-          ],
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              const CustomTextField(
+                  hintTxt: 'example@email.com', lableTxt: 'Email', mode: false),
+              const SizedBox(
+                height: 10,
+              ),
+              const CustomTextField(
+                hintTxt: '',
+                lableTxt: 'Password',
+                mode: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        shadowColor: Colors.white,
+                        backgroundColor: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                    height: 70,
+                    width: double.infinity,
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: 'Dont have an account?',
+                        style: TextStyle(color: Colors.grey[600]),
+                        children: [
+                          TextSpan(
+                            text: ' Register Here',
+                            style: TextStyle(color: Colors.blueAccent),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => OnBoard()));
+                              },
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
